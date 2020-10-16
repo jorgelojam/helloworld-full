3d552e5afdf4797435dab60ad6c06ae694543d08
Jorge Loja <jorgelojam@gmail.com>
2020-10-15 22:39:59 -0500

    Consulta SQL

--- src/main/java/ec/jtux/services/ParametrosRepo.java
+++ src/main/java/ec/jtux/services/ParametrosRepo.java
@@ -1,6 +1,10 @@
 package ec.jtux.services;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -59,7 +63,7 @@ public class ParametrosRepo {
 
     @GET
     @Path("/{codigo}")
-    @Produces({MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN})
     @Counted(description = "Contador parametro", absolute = true)
 	@Timed(name = "parametro-time", description = "Tiempo de procesamiento de parametro", unit = MetricUnits.MILLISECONDS, absolute = true)
     @Operation(description = "Consigue parametro del sistema", summary = "Parametro sistema")
@@ -83,6 +87,52 @@ public class ParametrosRepo {
             return Response.status(Response.Status.NOT_FOUND)
                 .entity("No hay resultados")
                 .build();
+        }
+    }
+
+    @GET
+    @Path("/query/{valor}")
+    @Produces({MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN})
+    @Counted(description = "Contador valor", absolute = true)
+	@Timed(name = "valor-time", description = "Tiempo de procesamiento de parametro valor", unit = MetricUnits.MILLISECONDS, absolute = true)
+    @Operation(description = "Consigue parametro del sistema por el valor", summary = "Parametro sistema por valor")
+	@APIResponses(value={
+	@APIResponse(responseCode = "200", description = "Parametro sistema",
+	             content = @Content(mediaType = MediaType.APPLICATION_JSON ,
+	                                schema = @Schema(implementation = Parametro.class))),
+    @APIResponse(responseCode = "400", description = "Parametro no existe",
+                content = @Content(mediaType = MediaType.TEXT_PLAIN ,
+                                    schema = @Schema(implementation = String.class)))}
+	)
+    public Response consultaParametroValor(@Parameter(description = "Valor de parametro", required = true)
+                                     @PathParam("valor") String valor){
+        log.info("Consulta SQL");
+        List<Parametro> result = new ArrayList<Parametro>();
+        try{
+            PreparedStatement ps = con.prepareStatement("select codigo,nombre,tipo,valor,eliminado,estado from parametros where valor = ?");
+            ps.setString(1, valor);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()){
+                Parametro p = new Parametro();
+                p.setCodigo(rs.getString(1));
+                p.setNombre(rs.getString(2));
+                p.setTipo(rs.getString(3));
+                p.setValor(rs.getString(4));
+                p.setEliminado(rs.getBoolean(5));
+                p.setEstado(rs.getBoolean(6));
+                result.add(p);
+            }
+            rs.close();
+            ps.close();
+        } catch (SQLException es){
+            log.error(es.getMessage());
+        }
+        if (result.size()==0){
+            return Response.status(Response.Status.NOT_FOUND)
+                .entity("No hay resultados")
+                .build();
+        }else{
+            return Response.ok(result).build();
         }
     }
     
